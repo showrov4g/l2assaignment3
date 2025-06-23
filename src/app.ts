ad8ec1bc87c0b63b175d242d2bbeb0160005616d
showrov4g <showrovg00@gmail.com>
2025-06-23 07:35:13 +0600

    server setup

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// API routes
+app.use('/books' );
+app.use('/borrow');
+// error handling 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    message: err.message || 'Something went wrong',
+    success: false,
+    error: err,
+  });
+});
+
+// Connect to MongoDB and start server
+const main = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://learning:7CZIO6IRQ7D63BZ4@cluster0.23lvn.mongodb.net/advance-to-do-app?retryWrites=true&w=majority&appName=Cluster0');
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  } catch (error) {
+    console.error('Connection error', error);
+  }
+};
+
+main();
